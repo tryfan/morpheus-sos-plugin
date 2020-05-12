8e6b3a85415fdfca186c3bac3dd783ef9be2c580
tryfan <nick@celebic.net>
2020-05-12 14:49:56 -0400

    testing mysqldump

--- sos/plugins/morpheus_mysql.py
+++ sos/plugins/morpheus_mysql.py
@@ -72,9 +72,17 @@ class Morpheus(Plugin, RedHatPlugin, DebianPlugin, UbuntuPlugin):
                 mysql_socket = config.get('client', 'socket')
                 self.get_userpass()
                 os.environ['MYSQL_PWD'] = self.mysql_pass
+                ### Check size of current DB
+                # sizechecksql = (
+                #     "select SUM(size) \"total\" from (select SUM(data_length + index_length) as \"size\"  \n"
+                #     "                FROM information_schema.tables  GROUP BY table_schema) t1;")
+                # opts = "--user %s -S %s morpheus -sN -e %s" % (self.mysql_user, mysql_socket, sizechecksql)
+                # self.get_command_output()
+
+                command = "/opt/morpheus/embedded/bin/mysqldump"
                 opts = "--user %s -S %s --all-databases" % (self.mysql_user, mysql_socket)
                 name = "mysqldump_--all-databases"
-                self.add_cmd_output("mysqldump %s" % opts, suggest_filename=name)
+                self.add_cmd_output("%s %s" % (command, opts), suggest_filename=name)
                 # else:
                 #     self._log_warn("Could not dump Morpheus MySQL DB. Install python2-mysql")
 
